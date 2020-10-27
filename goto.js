5094727d7eadd6c4c62cdd4f1a585a42643ad055
nz13684 <nz13684@gmail.com>
2020-10-27 21:32:52 +0800

    1027

--- goto.js
+++ goto.js
@@ -1,5 +1,8 @@
+// 遍尋所有元素
 $("*").each(function (index, element) {
+    // 此元素被點選後執行
     $(this).click(function (e) { 
+        // 取得被點選元素的屬性：data-gt-target
         var target = $(this).attr("data-gt-target");
         var duration = $(this).attr("data-gt-duration");
         var offset = $(this).attr("data-gt-offset");
