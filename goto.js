876f9d1579e7a6c03a453cd8acc83c2d4825a7a9
nz13684 <nz13684@gmail.com>
2020-10-27 21:31:29 +0800

    1027

--- goto.js
+++ goto.js
@@ -1,7 +1,11 @@
 $("*").each(function (index, element) {
     $(this).click(function (e) { 
-        var target = $(this).attr(data-gt-target);
-        console.log(data-gt-target);
+        var target = $(this).attr("data-gt-target");
+        var duration = $(this).attr("data-gt-duration");
+        var offset = $(this).attr("data-gt-offset");
+        console.log( "目標" + target);
+        console.log( "時間" + duration);
+        console.log( "位移" + offset);
     });
     
 });
