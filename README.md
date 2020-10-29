cbc5a6c5ee2fcc8a72282dfbb30f04c544d73576
nz13684 <nz13684@gmail.com>
2020-10-29 20:06:52 +0800

    1029

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # WEB_1027_jQuery_GoTo
+
+#範例網站
+https://kid421.github.io/Web_HC_ZL_jQuery_GoTo/
+
+#CDN
+
+ˋˋˋ
+<!-- jQuery CDN -->'
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- 前往元素 -->
+<script src="https://github.com/KID421/Web_HC_ZL_jQuery_GoTo/blob/main/goTo.js"></script>
+
+ˋˋˋ
+#屬性說明
+
+屬性明稱         │屬性說明
+----------------│-------------------------
+data-gt-target  │要前往的目標元素，必須使用ID
+data-gt-duration│持續時間，單位為毫秒
+data-gt-offset  │位移，往上位移的值
--- goto.js
+++ goto.js
@@ -15,14 +15,14 @@ $("*").each(function (index, element) {
         //如果 目標有資料 才會執行 {} 內的程式
         //避免出現 undefine (未定義 - 不存在的資料)
         if (target) {
-            console.log("目標" + target);
-            console.log("時間" + duration);
-            console.log("位移" + offset);
+            //console.log("目標" + target);
+            //console.log("時間" + duration);
+            //console.log("位移" + offset);
 
             //上方位置 = 目標區塊.位移().上方位置
             var top = $(target).offset().top;
-            console.log("要前往元素的上方位置：" + top);
-            
+            //console.log("要前往元素的上方位置：" + top);
+
             // 網頁元素.動畫({ 上方捲動：指定元素 - 位移}，持續時間)
             // parseInt() 將文字轉為數字
             $("html").stop().animate({
@@ -34,6 +34,6 @@ $("*").each(function (index, element) {
 
 //避免動畫與使用者滾輪衝突
 //html 在滾動滾輪時 停止 html 所有效果
-$("html").on("mousewheel", function() {
+$("html").on("mousewheel", function () {
     $("html").stop();
 });
