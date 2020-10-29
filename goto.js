009d9b5339f62f10071a63398088be6e13d761fa
add3120 <62015397+add3120@users.noreply.github.com>
2020-10-29 19:02:34 +0800

    1029

--- goto.js
+++ goto.js
@@ -6,9 +6,22 @@ $("*").each(function (index, element) {
         var target = $(this).attr("data-gt-target");
         var duration = $(this).attr("data-gt-duration");
         var offset = $(this).attr("data-gt-offset");
-        console.log("目標" + target);
-        console.log("時間" + duration);
-        console.log("位移" + offset);
-    });
 
+
+        //JS 語法：判斷式
+        //if(條件) {程式區塊}
+        //當條件成立，會執行程式區塊
+
+        //如果 目標有資料 才會執行 {} 內的程式
+        //避免出現 undefine (未定義 - 不存在的資料)
+        if (target) {
+            console.log("目標" + target);
+            console.log("時間" + duration);
+            console.log("位移" + offset);
+
+            //上方位置 = 目標區塊.位移().上方位置
+            var top = $(target).offset().top;
+            console.log("要前往元素的上方位置：" + top);
+        }
+    });
 });
